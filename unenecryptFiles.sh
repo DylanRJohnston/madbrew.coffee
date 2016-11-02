12a8ca136d99a3ea668ec19bcd13471ab95d08f4
Dylan R. Johnston <Dylan.R.Johnston@gmail.com>
2016-11-02 18:55:09 +0800

    Updated SSL certs

--- unenecryptFiles.sh
+++ unenecryptFiles.sh
@@ -8,5 +8,5 @@ DIR="$(cd "$(dirname "$0")"; pwd)"
 # Set word spliting to newlines only, avoids splitting on file paths with spaces.
 IFS=$'\n'
 for FILE in $(find "${DIR}" -name '*.gpg'); do
-    gpg --use-agent --decrypt 1>"${FILE%.gpg}" 2>/dev/null < "$FILE"
+    gpg --use-agent --decrypt 1>"${FILE%.asc}" 2>/dev/null < "$FILE"
 done
